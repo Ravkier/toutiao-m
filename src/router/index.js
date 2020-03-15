8828b3aba235bc68b9fa03fee6fdd27f530b5b3d
zhouhaoqiang <zhouhaoqiang@163.com>
2020-03-15 15:07:52 +0800

    路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,57 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/home'
 
 Vue.use(VueRouter)
+// 按需加载 一级路由和二级路由
+const Layout = () => import('@/views/layout')
+const Home = () => import('@/views/home')
+const question = () => import('@/views/question')
+const video = () => import('@/views/video')
+const user = () => import('@/views/user')
+// 其他一级路由
+const profile = () => import('@/views/user/profile')
+const chat = () => import('@/views/user/chat')
+const login = () => import('@/views/login')
+const search = () => import('@/views/search')
+const result = () => import('@/views/search/result')
+const article = () => import('@/views/article')
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
+const routes = [{
+  path: '/',
+  name: 'Home',
+  component: Layout,
+  children: [{
+    path: '',
+    component: Home // 二级路由主页
+  }, {
+    path: '/question',
+    component: question // 二级路由问答
+  }, {
+    path: '/video',
+    component: video // 二级路由 视频
+  }, {
+    path: '/user',
+    component: user // 二级路由个人
+  }]
+}, {
+  path: '/user/profile',
+  component: profile // 一级路由编辑资料
+}, {
+  path: '/user/chat',
+  component: chat // 一级路由小智同学
+}, {
+  path: '/login',
+  component: login // 一级路由登录
+}, {
+  path: '/search',
+  component: search // 一级路由搜素
+}, {
+  path: '/search/result',
+  component: result // 一级路由搜索结果
+}, {
+  path: '/article',
+  component: article // 一级路由 文章编辑
+}
 ]
 
 const router = new VueRouter({
