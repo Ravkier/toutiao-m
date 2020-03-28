351b8a4815f962fbfb81bbeed121ab901bc12331
zhouhaoqiang <zhouhaoqiang@163.com>
2020-03-28 14:27:12 +0800

    完善

--- src/views/user/chat.vue
+++ src/views/user/chat.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="container">
     <van-nav-bar fixed left-arrow @click-left="$router.back()" title="小智同学"></van-nav-bar>
-    <div class="chat-list">
+    <div class="chat-list" ref="myList">
       <div class="chat-item " :class="{left: item.name === 'xz', right:item.name !== 'xz'}" v-for="(item,index) in list" :key="index">
         <van-image fit="cover" v-if="item.name === 'xz'" round :src="XZImg" />
         <div class="chat-pao">{{ item.msg }}</div>
@@ -22,6 +22,7 @@
 </template>
 
 <script>
+import Vue from 'vue'
 import XZImg from '@/assets/1.jpg'
 import { mapState } from 'vuex'
 import io from 'socket.io-client' // 引入socket.io 客户端
@@ -35,6 +36,18 @@ export default {
     }
   },
   methods: {
+    // scrollTop(滚动条距离顶部的长度 ) = scrollHeight 整个屏幕
+    scrollBottom () {
+      // vuejs 数据变化 => 异步 => 试图更新
+      // this.$nextTick(() => {
+      //   // 上一步的结果已经完成更新
+      //   this.$refs.myList.scrollTop = this.$refs.myList.scrollHeight
+      // })
+      Vue.nextTick(() => {
+        // 上一步的结果已经完成更新
+        this.$refs.myList.scrollTop = this.$refs.myList.scrollHeight
+      })
+    },
     async send () {
       if (!this.value) return false // 如果字符串为空 就结束
       // 不为空
@@ -50,6 +63,7 @@ export default {
       // 发送完消息后
       this.value = '' // 清空输入框
       this.loading = false // 关掉状态
+      this.scrollBottom()
     }
   },
   computed: {
@@ -71,7 +85,13 @@ export default {
     this.socket.on('message', data => {
       // data 就是 客户服务器回复的消息
       this.list.push({ ...data, name: 'xz' })
+      this.scrollBottom()
     })
+  },
+  // 实例销毁之前
+  beforeDestroy () {
+    // 把websocket 连接关掉
+    this.socket.close() // 直接关闭
   }
 }
 </script>
